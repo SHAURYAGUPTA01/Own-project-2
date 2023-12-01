bf2a103ec9f05bb8692eba09714c007828d3ae56
SHAURYAGUPTA01 <86559522+SHAURYAGUPTA01@users.noreply.github.com>
2023-12-01 20:51:55 +0530

    Update bullets.js

--- bullets.js
+++ bullets.js
@@ -12,13 +12,6 @@ AFRAME.registerComponent("bullets", {
             radius: 1,
           });
   
-          bullet.setAttribute("material", "color", "black");
-          bullet.setAttribute("position", {
-            x: pos.x,
-            y: pos.y+1.6,
-            z: pos.z-0.08,
-          });
-  
           bullet.setAttribute("velocity", direction.multiplyScalar(-20));
           var scene = document.querySelector("#scene");
           bullet.setAttribute("dynamic-body", {
@@ -26,22 +19,9 @@ AFRAME.registerComponent("bullets", {
             mass: "0",
           });
           bullet.setAttribute("visible", false);
-          bullet.addEventListener("collide", this.removeBullet);
           scene.appendChild(bullet);
         }
       });
-    },
-    removeBullet: function (e) {
-      var scene = document.querySelector("#scene");
-      var element = e.detail.target.el;
-      var elementHit = e.detail.body.el;
-  
-      var pos = element.getAttribute("position")
-      var rotate = elementHit.getAttribute("rotation")
-  
-      element.removeEventListener("collide", this.removeBullet);
-   
-      scene.removeChild(element);
     },
   });
   
